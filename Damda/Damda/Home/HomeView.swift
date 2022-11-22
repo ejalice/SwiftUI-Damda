3641b46013bc6dfedcf7491fd0cc86d68780fa66
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-11-22 19:31:27 +0900

    [Feat] #1 HomeView 생성 후 뷰 색상 변경

---
+++ Damda/Damda/Home/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  Damda
+//
+//  Created by 이창형 on 2022/11/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.damdaBackGround.ignoresSafeArea()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
