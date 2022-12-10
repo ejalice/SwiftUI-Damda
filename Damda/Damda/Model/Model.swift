633496c12b90ad8968cc67700cf86ad5b4112b94
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-12-10 17:19:40 +0900

    [Feat] #4 각 항목 별 Model 추가

--- Damda/Damda/Model/Model.swift
+++ Damda/Damda/Model/Model.swift
@@ -9,5 +9,7 @@ import Foundation
 
 struct Model {
     // TODO: 코어데이터에서 데이터 받아오기
-    let bottleImageArray = ["1", "2", "3", "4", "5", "6"]
+    let ripeningBottleImageArray = ["1", "2", "3", "4", "5", "6"]
+    let maturedBottleImageArray = ["1", "2", "3", "4", "5"]
+    let mysteryBottleImageArray = ["1", "2", "3", "4"]
 }
