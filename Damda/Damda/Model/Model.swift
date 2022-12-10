a4f9e65cc129e8c0ae3716717f793fe664e0ef1f
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-12-10 17:01:22 +0900

    [Feat] #4 LibraryView에 들어갈 사진 Model 생성

---
+++ Damda/Damda/Model/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  Damda
+//
+//  Created by 이창형 on 2022/12/09.
+//
+
+import Foundation
+
+struct Model {
+    // TODO: 코어데이터에서 데이터 받아오기
+    let bottleImageArray = ["1", "2", "3", "4", "5", "6"]
+}
