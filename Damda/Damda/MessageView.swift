bb88966f99f057c4ef69e6c23da91a1f42f02400
EunJI <83203198+ejalice@users.noreply.github.com>
2023-02-27 22:54:18 +0900

    [Feat] #10 Keyboard Multiline and FirstRespond

--- Damda/Damda/MessageView.swift
+++ Damda/Damda/MessageView.swift
@@ -15,6 +15,11 @@ struct MessageView: View {
     @State var date = Date()
     
     @State var text: String = ""
+    @State var isAnimating  = false
+    @State var keyboardInput: String = ""
+    @State var inputEntered = false
+    @State var containerHeight: CGFloat = 0
+    
     
     var damdaMessage: [String] = [
         "잊지 않고 ‘담다’을 방문해주셔서 감사합니다",
@@ -39,37 +44,33 @@ struct MessageView: View {
                     Spacer(minLength: 20)
                     
                     ForEach(0..<3){ num in
-                        Group {
+                        Group { //~
                             if showFirstMessage[num] {
                                 if (num != 2) {
                                     DamdaText(damdaMessage[num])
-                                } else {
-                                    if !dateSelected {
+                                } else { // 날짜 선택하는 버튼들 띄움.
+                                    if !dateSelected { // 날짜 선택 아직 안함.
                                         DateView(strengthenScroll: $strengthenScroll, date: $date, dateSelected: $dateSelected)
                                     }
 
-                                    if dateSelected{
+                                    if dateSelected{ // 날짜 선택 완료.
                                         UserText(dateFormatter.string(from: date))
+                                        DamdaText(damdaMessage[3])
                                     }
                                     
                                 }
                             } else {
                                 DamdaText(damdaMessage[num]).hidden()
                             }
-                        }
+                        } //~
                         .onAppear {
                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5 * Double((num+1))) {
                                 self.showFirstMessage[num].toggle()
+                                print(num)
                             }
                         }
                         
                     }
-                    
-                    if dateSelected {
-                        DamdaText(damdaMessage[3])
-                        // TextField View?
-                    }
-                    
                     
                     Spacer()
                     
@@ -77,9 +78,37 @@ struct MessageView: View {
                 .border(.blue)
                 .frame(width: UIScreen.main.bounds.width, height: strengthenScroll ? 670 : 383)
                 .padding(.top, 1)
+                
                 Spacer()
-            }
+                
+                if dateSelected {
+                    HStack {
+//                        TextField(
+//                            "텍스트를 입력하세요",
+//                            text: $nickname
+//                        )
+//                        FirstResponderTextField(text: $keyboardInput)
+                        AutoSizingTF(hint: "Enter Text PLZ", text: $keyboardInput, containerHeight: $containerHeight, onEnd: {
+                            // Do when keyboard closed ..
+                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        })
+                            .padding(.horizontal)
+                            .background(Color.white)
+                            .frame(height: containerHeight <= 120 ?containerHeight : 120 )
+                        
+                        Button {
+                            inputEntered = true
+                            print(inputEntered)
+                        } label: {
+                            Image(systemName: "paperplane.fill")
+                        }
+                    }
+                }
+                Spacer()
             .background(Color.damdaBackGround)
+            
+            
+            }
         }
     }
 }
@@ -87,5 +116,139 @@ struct MessageView: View {
 struct MessageView_Previews: PreviewProvider {
     static var previews: some View {
         MessageView()
+    }
+}
+
+//struct FirstResponderTextField: UIViewRepresentable {
+//
+//    @Binding var text: String
+//
+//    class Coordinator: NSObject, UITextFieldDelegate {
+//
+//        @Binding var text: String
+//        var becameFirstResponder = false
+//
+//        init(text: Binding<String>) {
+//            self._text = text
+//        }
+//
+//        func textFieldDidChangeSelection(_ textField: UITextField) {
+//            text = textField.text ?? ""
+//        }
+//    }
+//
+//    func makeCoordinator() -> Coordinator {
+//        return Coordinator(text: $text)
+//    }
+//
+//    func makeUIView(context: Context) -> some UIView {
+//        let textField = UITextField()
+//        textField.delegate = context.coordinator
+//        return textField
+//    }
+//
+//    func updateUIView(_ uiView: UIViewType, context: Context) {
+//        if !context.coordinator.becameFirstResponder {
+//            uiView.becomeFirstResponder()
+//            context.coordinator.becameFirstResponder = true
+//        }
+//    }
+//}
+
+
+// https://www.youtube.com/watch?v=Jf8SzGLaRdA
+struct AutoSizingTF: UIViewRepresentable {
+    
+    var becameFirstResponder = false
+
+    var hint: String
+    @Binding var text: String
+    @Binding var containerHeight: CGFloat
+    var onEnd: ()->()
+    
+    func makeCoordinator() -> Coordinator {
+        return AutoSizingTF.Coordinator(parent: self)
+    }
+
+    func makeUIView(context: Context) -> UITextView {
+        
+        let textView = UITextView()
+        // Displaying text as hint
+        textView.text = hint
+        textView.textColor = .gray
+        
+        textView.font = .systemFont(ofSize: 20)
+        
+        // setting delegate
+        textView.delegate = context.coordinator
+        
+        // Input Accessory View ..
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        
+        toolBar.barStyle = .default
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: context.coordinator, action: #selector(context.coordinator.closeKeyBoard))
+        
+        toolBar.items = [spacer, doneButton]
+        toolBar.sizeToFit()
+        
+        textView.inputAccessoryView = toolBar
+        
+        return textView
+    }
+    
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        if !context.coordinator.becameFirstResponder {
+            uiView.becomeFirstResponder()
+            context.coordinator.becameFirstResponder = true
+        }
+        // Starting Text Field Height ..
+        DispatchQueue.main.async {
+            if containerHeight == 0 {
+                containerHeight = uiView.contentSize.height
+            }
+        }
+    }
+    
+    class Coordinator: NSObject, UITextViewDelegate {
+        
+        var becameFirstResponder = false
+
+        // To read all parent properties ...
+        var parent: AutoSizingTF
+        
+        init(parent: AutoSizingTF) {
+            self.parent = parent
+        }
+        // Keyboard close @objc function ..
+        @objc func closeKeyBoard() {
+            parent.onEnd()
+        }
+        
+        func textViewDidBeginEditing(_ textView: UITextView) {
+            // checking if text box is empty ..
+            // is so then clearing the hint ..
+            if textView.text == parent.hint {
+                textView.text = ""
+                textView.textColor = UIColor(.blue)
+            }
+        }
+        
+        // updating text in SwiftUI View
+        func textViewDidChange(_ textView: UITextView) {
+            parent.text = textView.text
+            parent.containerHeight = textView.contentSize.height
+        }
+        
+        // On End checking if textbox is empty
+        // if so then put hint..
+        func textViewDidEndEditing(_ textView: UITextView) {
+            if textView.text == "" {
+                textView.text = parent.hint
+                textView.textColor = .gray
+            }
+        }
     }
 }
